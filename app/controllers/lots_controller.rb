6705845afd1ad53ae8f5b7456058435023035703
André Luiz Gava <andrelzgava@gmail.com>
2016-06-20 09:43:35 -0300

    Interface and behavior on save

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -1,34 +1,26 @@
 class LotsController < ApplicationController
   before_action :set_lot, only: [:show, :edit, :update, :destroy]
 
-  # GET /lots
-  # GET /lots.json
   def index
     @lots = Lot.all
   end
 
-  # GET /lots/1
-  # GET /lots/1.json
   def show
   end
 
-  # GET /lots/new
   def new
     @lot = Lot.new
   end
 
-  # GET /lots/1/edit
   def edit
   end
 
-  # POST /lots
-  # POST /lots.json
   def create
     @lot = Lot.new(lot_params)
 
     respond_to do |format|
       if @lot.save
-        format.html { redirect_to @lot, notice: 'Lot was successfully created.' }
+        format.html { redirect_to lots_path, notice: 'Lot was successfully created.' }
         format.json { render :show, status: :created, location: @lot }
       else
         format.html { render :new }
@@ -37,12 +29,10 @@ class LotsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lots/1
-  # PATCH/PUT /lots/1.json
   def update
     respond_to do |format|
       if @lot.update(lot_params)
-        format.html { redirect_to @lot, notice: 'Lot was successfully updated.' }
+        format.html { redirect_to lots_path, notice: 'Lot was successfully updated.' }
         format.json { render :show, status: :ok, location: @lot }
       else
         format.html { render :edit }
@@ -51,8 +41,6 @@ class LotsController < ApplicationController
     end
   end
 
-  # DELETE /lots/1
-  # DELETE /lots/1.json
   def destroy
     @lot.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class LotsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_lot
       @lot = Lot.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def lot_params
       params.require(:lot).permit(:name, :description, :property_id)
     end
--- app/controllers/reproductions_controller.rb
+++ app/controllers/reproductions_controller.rb
@@ -1,33 +1,25 @@
 class ReproductionsController < ApplicationController
   before_action :set_reproduction, only: [:show, :edit, :update, :destroy]
 
-  # GET /reproductions
-  # GET /reproductions.json
   def index
     @reproductions = Reproduction.all
   end
 
-  # GET /reproductions/1
-  # GET /reproductions/1.json
   def show
   end
 
-  # GET /reproductions/new
   def new
     @reproduction = Reproduction.new
   end
 
-  # GET /reproductions/1/edit
   def edit
   end
 
-  # POST /reproductions
-  # POST /reproductions.json
   def create
     @reproduction = Reproduction.new(reproduction_params)
     respond_to do |format|
       if @reproduction.save
-        format.html { redirect_to @reproduction, notice: 'Reproduction was successfully created.' }
+        format.html { redirect_to reproductions_path, notice: 'Reproduction was successfully created.' }
         format.json { render :show, status: :created, location: @reproduction }
       else
         format.html { render :new }
@@ -36,12 +28,10 @@ class ReproductionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reproductions/1
-  # PATCH/PUT /reproductions/1.json
   def update
     respond_to do |format|
       if @reproduction.update(reproduction_params)
-        format.html { redirect_to @reproduction, notice: 'Reproduction was successfully updated.' }
+        format.html { redirect_to reproductions_path, notice: 'Reproduction was successfully updated.' }
         format.json { render :show, status: :ok, location: @reproduction }
       else
         format.html { render :edit }
@@ -50,8 +40,6 @@ class ReproductionsController < ApplicationController
     end
   end
 
-  # DELETE /reproductions/1
-  # DELETE /reproductions/1.json
   def destroy
     @reproduction.destroy
     respond_to do |format|
@@ -61,12 +49,10 @@ class ReproductionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_reproduction
       @reproduction = Reproduction.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def reproduction_params
       params.require(:reproduction).permit(:heat, :insemination, :regress, :abortion, :parturition, :mother_id, :father_id)
     end
