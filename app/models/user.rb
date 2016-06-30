7b70e14f733a9bcb8d1581de894efa302fbebe4c
André Luiz Gava <andrelzgava@gmail.com>
2016-06-30 11:56:49 -0300

    User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "30x30>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "30x30>" }, default_url: "/images/:style/missing_user.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   has_paper_trail
