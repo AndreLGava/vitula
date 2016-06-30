10d5d0f2f3e4ffc1f2751e49d8130ec990e3233a
André Luiz Gava <andrelzgava@gmail.com>
2016-06-30 17:33:31 -0300

    Notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :set_notification, only: [:destroy]
 
   def index
-    @notifications = Notification.all
+    @notifications = Notification.where(user_id: current_user.id)
   end
 
   def new
@@ -14,7 +14,7 @@ class NotificationsController < ApplicationController
 
     respond_to do |format|
       if @notification.save
-        format.html { redirect_to @notification, notice: 'Notification was successfully created.' }
+        format.html { redirect_to @notification, notice: I18n.t('crud.saved') }
         format.json { render :show, status: :created, location: @notification }
       else
         format.html { render :new }
@@ -26,18 +26,16 @@ class NotificationsController < ApplicationController
   def destroy
     @notification.destroy
     respond_to do |format|
-      format.html { redirect_to notifications_url, notice: 'Notification was successfully destroyed.' }
+      format.html { redirect_to notifications_url, I18n.t('crud.destroyed') }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_notification
       @notification = Notification.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def notification_params
       params.require(:notification).permit(:title, :description, :read, :limit, :user_id)
     end
