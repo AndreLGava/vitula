36e363e529d605f65a3ead93cd012431631a6801
André Luiz Gava <andrelzgava@gmail.com>
2016-07-11 16:15:20 -0300

    validates fields for development and production

--- app/models/development.rb
+++ app/models/development.rb
@@ -1,3 +1,7 @@
 class Development < ActiveRecord::Base
   belongs_to :animal
+
+  validates :weight, presence: true
+  validates :height, presence: true
+
 end
--- app/models/production.rb
+++ app/models/production.rb
@@ -1,3 +1,6 @@
 class Production < ActiveRecord::Base
   belongs_to :animal
+
+  validates :amount, presence: true
+
 end
