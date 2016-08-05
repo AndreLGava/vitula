af270a5f2f1a8ff6b9a124e104b2a707c2495e0a
André Luiz Gava <andrelzgava@gmail.com>
2016-08-05 11:34:35 -0300

    doing

--- config/application.rb
+++ config/application.rb
@@ -26,12 +26,13 @@ module Vitula
 
 
     config.action_mailer.delivery_method = :smtp
+
     config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :domain               => 'gmail.com',
       :user_name            => 'vitulasystem',
-      :password             => 'senha',
+      :password             => 'uniedusystem',
       :authentication       => 'plain',
       :enable_starttls_auto => true
     }
