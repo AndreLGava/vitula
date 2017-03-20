3d114023436b483c6c0c4ff88b426f6e7ed3f13a
André Luiz Gava <andrelzgava@gmail.com>
2017-03-19 22:49:42 -0300

    Reformulando

--- app/controllers/glebes_controller.rb
+++ app/controllers/glebes_controller.rb
@@ -86,6 +86,6 @@ class GlebesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def glebe_params
-      params.require(:glebe).permit(:name, :purpose, :area, :property_id)
+      params.require(:glebe).permit(:name, :purpose, :area, :property_id, :inactive)
     end
 end
