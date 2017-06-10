7cb250039497c23e74e684465abf1362f6c6d9e7
André Luiz Gava <andrelzgava@gmail.com>
2017-06-10 19:41:05 -0300

    go

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require bootstrap-sprockets
-//= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require jquery.validate
 //= require jquery.validate.additional-methods
+//= require bootstrap-sprockets
 //= require bootstrap-datepicker
 //= require jquery.jcrop
 //= require papercrop
