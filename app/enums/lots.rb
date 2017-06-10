1fa29c2749cb3808338bb7b69b246d942190b10b
André Luiz Gava <andrelzgava@gmail.com>
2017-06-10 19:20:20 -0300

    go

--- app/enums/lots.rb
+++ app/enums/lots.rb
@@ -1,3 +1,3 @@
 class Lots < ActiveRecord::Base
-  enum lots: [ terneiros: 1, novilhos_abate: 2, novilhas: 3, vacas_lactantes: 4, vacas_secas: 5, reprodutores: 6 ]
+  enum lots: {terneiros: 1, novilhos_abate: 2, novilhas: 3, vacas_lactantes: 4, vacas_secas: 5, reprodutores: 6 }
 end
